7312bcbbf1d4a7e66ad06d68b7383d3c246b6e0e
Atte <pitkasenatte@gmail.com>
2024-02-02 16:04:04 +0100

    Adds character knockback on shot and adds sounds to revolver

--- Items/Weapons/Weapon.cs
+++ Items/Weapons/Weapon.cs
@@ -4,20 +4,28 @@ using System;
 public partial class Weapon : HeldItem
 {
     [Export] public WeaponResource stats;
-    [Export] private RayCast3D bulletRay;
+    [Export] private RayCast3D _bulletRay;
+    [Export] private AudioStreamPlayer3D _audioPlayer;
 
     public override void LeftClick()
     {
         GD.Print("Bang!");
-        dynamic collider = bulletRay.GetCollider();
-        var bulletTravelDirection = (bulletRay.GetCollisionPoint() - GlobalPosition).Normalized();
+        dynamic collider = _bulletRay.GetCollider();
+        var bulletTravelDirection = (_bulletRay.GetCollisionPoint() - GlobalPosition).Normalized();
+        Rpc(nameof(PlaySound));
         if (collider is Player player)
         {
-            player.RpcId(int.Parse(player.Name), nameof(player.Hit), stats.Damage, bulletTravelDirection);
+            player.RpcId(int.Parse(player.Name), nameof(player.Hit), stats.Damage, bulletTravelDirection * stats.Damage / 3);
         }
         else if (collider is Item || collider is Bone)
         {
             collider.Rpc(nameof(collider.Hit), stats.Damage, bulletTravelDirection);
         }
+    }
+
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    public void PlaySound()
+    {
+        _audioPlayer.Play();
     }
 }
