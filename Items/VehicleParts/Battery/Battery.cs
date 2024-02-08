a62694966c360320001f48eada5ea645991356fe
Atte <pitkasenatte@gmail.com>
2024-02-08 17:03:49 +0100

    Implements battery and its installation

--- Items/VehicleParts/Battery/Battery.cs
+++ Items/VehicleParts/Battery/Battery.cs
@@ -3,34 +3,5 @@ using System;
 
 public partial class Battery : CarPart
 {
-	public int charge = 100;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (GetCondition() < 1)
-		{
-			charge = 0;
-		}
-	}
-
-	public void Decharge()
-	{
-		charge--;
-	}
-
-	public void Recharge()
-	{
-		charge++;
-	}
-
-	public int GetCharge()
-	{
-		return charge;
-	}
 }
