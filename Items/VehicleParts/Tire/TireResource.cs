ba117469ad8b8d894a316bb704befdb731513f31
Atte <pitkasenatte@gmail.com>
2024-02-11 18:42:21 +0100

    Implements tire installation

---
+++ Items/VehicleParts/Tire/TireResource.cs
@@ -0,0 +1,15 @@
+using Godot;
+using Godot.Collections;
+
+[GlobalClass]
+public partial class TireResource : Resource
+{
+    [Export] public float TireRadius { get; set; } = 0.35f;
+    [Export] public float TireWidth { get; set; } = 0.2f;
+    // Make sure you provide a parameterless constructor.
+    // In C#, a parameterless constructor is different from a
+    // constructor with all default values.
+    // Without a parameterless constructor, Godot will have problems
+    // creating and editing your resource via the inspector.
+    public TireResource() { }
+}
