e7d173b3d3ddb056592a75b4f45a4f5b1aa89742
Atte Pitkänen <pitkasenatte@gmail.com>
2024-07-25 16:20:17 +0200

    Removes old versions of ilari and adds new one with roads

--- Vehicles/Seat/Seat.cs
+++ Vehicles/Seat/Seat.cs
@@ -4,76 +4,81 @@ using System.Linq;
 
 public partial class Seat : CarPart
 {
-	public Vehicle vehicle;
-	public Marker3D seatPosition;
-	private Player _seatedPlayer;
-	private int _seatedPlayerId = 0;
-	public bool occupied = false;
-	[Export] public bool isDriverSeat = false;
+    public Vehicle vehicle;
+    public Marker3D seatPosition;
+    private Player _seatedPlayer;
+    private int _seatedPlayerId = 0;
+    public bool occupied = false;
+    [Export] public bool isDriverSeat = false;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		seatPosition = GetNode<Marker3D>("SeatPosition");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        seatPosition = GetNode<Marker3D>("SeatPosition");
+    }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if (_seatedPlayer is null && _seatedPlayerId != 0)
-		{
-			_seatedPlayer = GetPlayer(_seatedPlayerId);
-		}
-		else if (_seatedPlayerId == 0)
-		{
-			_seatedPlayer = null;
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_seatedPlayer is null && _seatedPlayerId != 0)
+        {
+            _seatedPlayer = GetPlayer(_seatedPlayerId);
+        }
+        else if (_seatedPlayerId == 0)
+        {
+            _seatedPlayer = null;
+        }
+    }
 
-		if (_seatedPlayer is not null)
-		{
-			MovePassenger(_seatedPlayer);
-		}
-	}
+    public override void _Process(double delta)
+    {
+        if (_seatedPlayer is not null)
+        {
+            MovePassenger(_seatedPlayer);
+        }
+    }
 
-	public Vehicle GetVehicle()
-	{
-		return vehicle;
-	}
 
-	public long GetSeatedPlayerId()
-	{
-		return _seatedPlayerId;
-	}
 
-	public Player GetPlayer(long id)
-	{
-		var players = GetTree().GetNodesInGroup("Player");
-		if (players.Count() < 1) return null;
-		return players.First(player => ((Player)player).id == id) as Player;
-	}
+    public Vehicle GetVehicle()
+    {
+        return vehicle;
+    }
 
-	public void MovePassenger(Player player)
-	{
-		player.MovePlayer(seatPosition.GlobalPosition, seatPosition.GlobalRotation);
-	}
+    public long GetSeatedPlayerId()
+    {
+        return _seatedPlayerId;
+    }
 
-	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
-	public void Sit(int playerId)
-	{
-		if (_seatedPlayerId == 0)
-		{
-			_seatedPlayerId = playerId;
-			occupied = true;
-		}
-	}
+    public Player GetPlayer(long id)
+    {
+        var players = GetTree().GetNodesInGroup("Player");
+        if (players.Count() < 1) return null;
+        return players.First(player => ((Player)player).id == id) as Player;
+    }
 
-	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
-	public void Stand()
-	{
-		GD.Print("Stand up");
-		if (_seatedPlayerId != 0)
-		{
-			_seatedPlayerId = 0;
-			occupied = false;
-		}
-	}
+    public void MovePassenger(Player player)
+    {
+        player.MovePlayer(seatPosition.GlobalPosition, seatPosition.GlobalRotation);
+    }
+
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    public void Sit(int playerId)
+    {
+        if (_seatedPlayerId == 0)
+        {
+            _seatedPlayerId = playerId;
+            occupied = true;
+        }
+    }
+
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    public void Stand()
+    {
+        GD.Print("Stand up");
+        if (_seatedPlayerId != 0)
+        {
+            _seatedPlayerId = 0;
+            occupied = false;
+        }
+    }
 }
